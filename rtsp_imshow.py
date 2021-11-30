f04f5cf81379b0bd76de9ac2fe5aa3991182a0d6
KNChiu <y9760210@gmail.com>
2021-11-30 12:34:23 +0800

    RTSP即時串流、避免斷線

--- rtsp_imshow.py
+++ rtsp_imshow.py
@@ -1,31 +1,43 @@
 import time
 import cv2 
 import time
-start = time.time()
 
 RTSP = r"rtsp://admin:mirdc83300307@192.168.0.237:554/stream1"
-# RTSP = r"rtsp://admin:mirdc83300307@192.168.0.237:554/stream2"
+# # RTSP = r"rtsp://admin:mirdc83300307@192.168.0.237:554/stream2"
 
-
-cap = cv2.VideoCapture(RTSP)
 
 start = time.time()
 
-while True:
-    ret, frame = cap.read()
+def work_with_captured_video(camera):
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            print("Camera is disconnected!")
+            camera.release()
+            return False
+        else:
+            cv2.imshow('frame', frame)
 
-    if ret:
-        cv2.imshow("frame",frame)
-        ch = cv2.waitKey(1)
-        
-        if ch == 27 or ch == ord('q') or ch == ord('Q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    else: 
-        break
+    return True
 
-    end = time.time()
-    print("FPS：%f " % (1/(end - start)))
 
-cv2.destroyAllWindows()
-cap.release()
+while True:
+    camera = cv2.VideoCapture(RTSP)
+    if camera.isOpened():
+        print('Camera is connected')
+        #call function
+        response = work_with_captured_video(camera)
+        if response == False:
+            time.sleep(10)
+            continue
+        else:
+            break
+    else:
+        print('Camera not connected')
+        camera.release()
+        time.sleep(10)
+        continue
+
 print(time.time()-start)
